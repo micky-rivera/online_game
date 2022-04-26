883c264ab2960be29bf5ab23539e10cf7ca025e0
unknown <mikhailrivera12@gmail.com>
2022-04-26 22:53:39 +0200

    files served

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  const client = path.join(__dirname, '..', 'client');
+  const public = path.join(__dirname, '..', 'client', 'public');
+  app.use(express.static(client));
+  app.use(express.static(public));
+  res.sendFile(path.join(__dirname, '..', 'client', 'public' , 'index.html'));
+});
+
+module.exports.app = app;
+app.listen(process.env.PORT || 8080);
